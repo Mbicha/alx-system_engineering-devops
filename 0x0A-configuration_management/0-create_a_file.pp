1ade9aa88bc09608a3e35851d046bcd64308ea9b
Mbicha <mbithicharlse@gmail.com>
2022-09-17 21:31:01 +0300

    Completed the tasks

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+#script to to create a file in /tmp directory
+file{ '/tmp/school':
+  ensure  =>'present',
+  content => 'I love Puppet',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+}
