2d63b6b694f0fba8d4abb328f8af059583d59bae
Mbicha <mbithicharlse@gmail.com>
2022-10-18 00:44:45 +0300

    First commit

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    userId = argv[1]
+    user = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{0}"
+        .format(userId))
+
+    EMPLOYEE_NAME = user.json().get('name')
+
+    todo_tasks = requests.get("https://jsonplaceholder.typicode.com/todos")
+    NUMBER_OF_DONE_TASKS = 0
+    TOTAL_NUMBER_OF_TASKS = 0
+
+    for task in todo_tasks.json():
+        if task.get('userId') == int(userId):
+            TOTAL_NUMBER_OF_TASKS += 1
+            if task.get('completed'):
+                NUMBER_OF_DONE_TASKS += 1
+
+    print('Employee {} is done with tasks({}/{}):'
+          .format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
+
+    print('\n'.join(["\t " + task.get('title') for task in todo_tasks.json()
+          if task.get('userId') == int(userId) and task.get('completed')]))
